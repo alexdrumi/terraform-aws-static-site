9d0c3a00c37c2570506c3d84c076a814086afd28
bmajor <mbencus89@gmail.com>
2025-02-09 16:57:12 +0100

    basic file running on EC2 instances

--- static.sh
+++ static.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
 set -e
 exec > >(tee /var/log/user_data.log | logger -t user-data) 2>&1
 
-# Update packages
+#always update pcks first on linux
 sudo apt-get update -y
 
-# Install Apache
+#install apache, nginx would also suffice
 sudo apt-get install -y apache2
 
-# Start Apache service
+#start apache
 sudo systemctl start apache2
 sudo systemctl enable apache2
 
-# Set up a basic webpage
+#a joke of a html
 echo "Hello, World!" > /var/www/html/index.html
 
-# Set up a health check
+#we will see if this works for health check, had a bug before with nat gateway for being able to download
 echo "healthy" > /var/www/html/health
+
